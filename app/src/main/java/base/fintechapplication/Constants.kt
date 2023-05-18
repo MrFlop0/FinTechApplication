a59a8b4f5f6fb12a799f023781f5c6a5d282002c
Kaplenkov Alexander <92748651+MrFlop0@users.noreply.github.com>
2023-05-18 14:04:57 +0300

    Update Constants.kt

--- app/src/main/java/base/fintechapplication/Constants.kt
+++ app/src/main/java/base/fintechapplication/Constants.kt
@@ -6,7 +6,7 @@ class Constants {
         const val tag_Favorite = "2"
         const val tag_Popular = "3"
         const val type = "TOP_100_POPULAR_FILMS"
-        const val key = "e30ffed0-76ab-4dd6-b41f-4c9da2b2735b"
+        const val key = "" // key for access to API goes here
         const val endpoint = "https://kinopoiskapiunofficial.tech/api/v2.2/"
         const val intentPopular = "Favorite"
         const val getMovieInfo = "get1"
